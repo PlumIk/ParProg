c8fd2b236ccf699bf2ccafd7932e438b2a700788
alex18209 <zmey25062000@gmail.com>
2022-02-01 22:08:46 +0600

    Добавлено: Изменил всё(полностью)

--- datawork/datagen/typeother/allvalues_int.py
+++ datawork/datagen/typeother/allvalues_int.py
@@ -4,6 +4,10 @@ from datawork.datagen.intercommfun import data_swapper
 
 class AllValuesInt(DataGennerInterface):
 
+    def __init__(self):
+        super(AllValuesInt, self).__init__()
+
+
     def gen_data(self, data: list) -> list:
         data = data_swapper(data)
         ret = list()
